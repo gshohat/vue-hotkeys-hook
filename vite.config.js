286411f69e8041e584ccc52012d634b02fc3ff95
gshohat <gilad.shohat@gmail.com>
2023-09-27 19:51:56 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.js"),
+      name: "vue-hotkeys-hook",
+      fileName: (format) => `hotkeys-hook.${format}.js`
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        }
+      }
+    },
+  },
+  plugins: [vue()],
+})
